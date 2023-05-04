32ba66d0514893ab6dcaee1cc907cd0206ad9810
floorismfounder <123405289+floorismfounder@users.noreply.github.com>
2023-05-04 09:00:29 -0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "runtime" {
+  type    = string
+  default = "nodejs14.x"
+}
+
+variable "memory_size" {
+  type    = number
+  default = 128
+}
+
+variable "s3_bucket" {
+  type    = string
+  default = "my-lambda-s3-bucket"
+}
+
+variable "function_name" {
+  type    = string
+  default = "my-lambda-function"
+}
+
+variable "handler" {
+  type    = string
+  default = "index.handler"
+}
+
+variable "timeout" {
+  type    = number
+  default = 10
+}
+
+variable "s3_key" {
+    type = string
+    default = "another_lambda_function.zip"
+}
